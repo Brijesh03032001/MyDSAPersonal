4ea12c47c03c816f8e6ced0e32fc05ad1cd471db
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-05-07 19:02:56 +0000

    Add LeetCode: 3748 - Sort Matrix by Diagonals (cpp)

--- problems/Medium/3748-sort-matrix-by-diagonals/solution_cpp.cpp
+++ problems/Medium/3748-sort-matrix-by-diagonals/solution_cpp.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
-    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        unordered_map<int, priority_queue<int>> maxHeaps;
-        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> minHeaps;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int key = i - j;
-                if (key < 0) minHeaps[key].push(grid[i][j]);
-                else maxHeaps[key].push(grid[i][j]);
+  vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        vector<vector<int>> copy = grid;
+        int n = grid.size();
+        // upward
+        int k=0;
+        while(k< n-1)
+        {
+            vector<int>sol;
+            for(int i=0;i<n-1-k;i++)
+            {
+                sol.push_back(grid[i][i+k+1]);
             }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int key = i - j;
-                if (key < 0) {
-                    grid[i][j] = minHeaps[key].top();
-                    minHeaps[key].pop();
-                } else {
-                    grid[i][j] = maxHeaps[key].top();
-                    maxHeaps[key].pop();
-                }
+            sort(sol.begin(), sol.end());
+            for(int i=0;i<n-1-k;i++)
+            {
+                copy[i][i+k+1] = sol[i];
             }
+            k++;
         }
-        return grid;
+        // downward
+        int k2=0;
+        while(k2< n-1)
+        {
+            vector<int>sol;
+            for(int i=0;i<n-1-k2;i++)
+            {
+                sol.push_back(grid[i+k2+1][i]);
+            }
+            sort(sol.rbegin(), sol.rend());
+            for(int i=0;i<n-1-k2;i++)
+            {
+                copy[i+k2+1][i] = sol[i];
+            }
+            k2++;
+        }
+        //diagonal
+        vector<int> solu ;
+        for(int i=0; i < n; i++)
+        {
+            solu.push_back(grid[i][i]);
+           
+        }
+        sort(solu.rbegin(), solu.rend());
+    
+        int l3=0;
+        for(int i=0; i < n; i++)
+        {
+            copy[i][i] = solu[l3];
+            l3++;
+           
+        }
+        return copy;
     }
 };
