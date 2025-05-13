c7909a396a74204d0130ccc6b3865da5533fa259
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-05-13 21:25:13 +0000

    Add LeetCode: 76 - Minimum Window Substring (cpp)

---
+++ problems/Hard/76-minimum-window-substring/solution_cpp.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> vc;
+        int le = s.size();
+        if( le < t.size()) return "";
+        for(int i=0;i<t.size();i++)
+        {
+            vc[t[i] - 'A'] ++;
+        }
+        int start_i= 0;
+        int counted_req = t.size();
+        int i =0 , j=0;
+        int windowSize = INT_MAX;
+        while(j < le)
+        {
+            
+            if( vc[s[j] - 'A'] > 0)
+            {
+                counted_req --;
+
+            }
+
+            vc[s[j]- 'A'] --;
+            while(counted_req == 0)
+            {
+                int te = j-i+1;
+                if( te < windowSize)
+                {
+                    start_i  = i;
+                    windowSize = te;
+                }
+                if(vc[s[i]- 'A'] >= 0)
+                {
+                    counted_req ++;
+                }
+                 vc[s[i]- 'A']++;
+                i++;
+            }
+            j++;
+        }
+        if(windowSize == INT_MAX)
+        {
+            return "";
+        } 
+        return s.substr(start_i, windowSize);
+
+
+    }
+};
