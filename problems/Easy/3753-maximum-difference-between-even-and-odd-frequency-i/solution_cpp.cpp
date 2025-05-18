1c17ca92a9bf0049381ed543c841d129c4412a3a
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-05-18 03:34:35 +0000

    Add LeetCode: 3753 - Maximum Difference Between Even and Odd Frequency I (cpp)

--- problems/Easy/3753-maximum-difference-between-even-and-odd-frequency-i/solution_cpp.cpp
+++ problems/Easy/3753-maximum-difference-between-even-and-odd-frequency-i/solution_cpp.cpp
@@ -1,5 +1,25 @@
-// LeetCode submission — code could not be fetched automatically.
-// To fill this in, open the submission on LeetCode and paste your code here.
-// Submission key: maximum-difference-between-even-and-odd-frequency-i__1749523339
-// Language: cpp
-// Status: Accepted
+class Solution {
+public:
+    int maxDifference(string s) {
+        unordered_map<char,int> mp;
+        for(int i=0;i< s.size();i++)
+        {
+            mp[s[i]]++;
+        }
+         int ev = INT_MAX;
+         int od = INT_MIN;
+
+        for(auto it : mp)
+        {
+             if(it.second % 2 == 0)
+             {
+                ev = min(ev, it.second);
+             }
+             else
+             {
+                od = max(od, it.second);
+             }
+        }
+        return od - ev;
+    }
+};
