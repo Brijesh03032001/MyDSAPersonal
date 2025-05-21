811f6ffdb7824c7b91fbd1aa552c2b2ce076126d
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-05-21 20:00:17 +0000

    Add LeetCode: 20 - Valid Parentheses (cpp)

--- problems/Easy/20-valid-parentheses/solution_cpp.cpp
+++ problems/Easy/20-valid-parentheses/solution_cpp.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.size();
-        stack<int>st;
-        for(int i=0;i<n;i++)
+        stack<char>st;
+        unordered_map<char,char> mp{{']', '['} , {'}', '{'} , {')', '('}};
+
+        for(int i=0;i<s.size();i++)
         {
-            char ch = s[i];
-            if( ch == '{' || ch == '[' || ch == '(')
-            {
-                st.push(ch);
-            }
-            else
-            {
-                if(st.empty()) return false;
-                char to = st.top();
-                if ( (ch == '}' && to == '{') ||
-                     (ch == ']' && to == '[') ||
-                     (ch == ')' && to == '(') )
+                if(st.empty() || s[i] == '[' || s[i] == '{' || s[i] == '(')
                 {
-                       st.pop();
-                } 
+                       st.push(s[i]);
+                }
+                else if(st.top() == mp[s[i]])
+                {
+                    st.pop();
+                }
                 else
                 {
                     return false;
                 }
-            }
+
         }
         if(!st.empty())
         {
             return false;
         }
-        return true;
+                 return true;
     }
 };
