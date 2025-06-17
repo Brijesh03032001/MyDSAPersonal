5740dc67a07d2439b9d69a1a02b399bbd0494a22
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-06-17 16:25:03 +0000

    Add LeetCode: 1755 - Defuse the Bomb (cpp)

--- problems/Easy/1755-defuse-the-bomb/solution_cpp.cpp
+++ problems/Easy/1755-defuse-the-bomb/solution_cpp.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
+       
+       if(k==0)
+       {
+        vector<int>ans(code.size(),0);
+        return ans;
+       }
+       else if(k>0)
+       {
+        vector<int> res;
+        long long su = 0;
+        for(int i=0;i<k;i++)
+        {
+            su += code[(i+1)];
+        }
+        res.push_back(su);
         int n = code.size();
-        vector<int> res(n, 0);
-        
-        if (k == 0) {
-            return res; // All elements are zero when k is zero
+        for(int j=1;j<n;j++)
+        {
+            su = su - code[j] + code[(k+j)%n];
+            res.push_back(su);
         }
-        
-        int start = (k > 0) ? 1 : n + k; // Start index for summation
-        int end = (k > 0) ? k : n - 1;  // End index for summation
-        
-        // Compute initial sum for the first window
-        int sum = 0;
-        for (int i = start; i <= end; i++) {
-            sum += code[i % n];
-        }
-        
-        for (int i = 0; i < n; i++) {
-            res[i] = sum;
-            // Slide the window
-            sum -= code[(start++) % n];
-            sum += code[(++end) % n];
-        }
-        
         return res;
+
+       }
+       else
+       {
+          vector<int>res;
+          int n = code.size();
+          int st = k + n;
+          long long su =0;
+          for(int i=st;i<n;i++)
+          {
+            su += code[i];
+          }
+          res.push_back(su);
+          for(int j=1;j<n;j++)
+          {
+              su = su - code[st%n] + code[(n+j-1)%n];
+              st++;
+              res.push_back(su);
+          }
+          return res;
+       }
+       vector<int>res;
+       return res;
     }
 };
