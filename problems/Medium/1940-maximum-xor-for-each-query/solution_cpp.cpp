89754a2285bc09ec2267c77ad8221d4788af1a2c
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-06-26 05:29:33 +0000

    Add LeetCode: 1940 - Maximum XOR for Each Query (cpp)

--- problems/Medium/1940-maximum-xor-for-each-query/solution_cpp.cpp
+++ problems/Medium/1940-maximum-xor-for-each-query/solution_cpp.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        int maxVal = (1 << maximumBit) - 1; 
+         int maxVal = (1 << maximumBit) - 1; // This is 2^maximumBit - 1
         int currentXor = 0;
-        vector<int> answer(nums.size());
+        std::vector<int> answer(nums.size());
         
-       
+        // Calculate the XOR of all elements in nums initially
         for (int num : nums) {
             currentXor ^= num;
         }
         
-       
+        // Iteratively calculate the answer for each query
         for (int i = 0; i < nums.size(); ++i) {
-            answer[i] = currentXor ^ maxVal; 
-            currentXor ^= nums[nums.size() - 1 - i];
+            answer[i] = currentXor ^ maxVal; // Calculate k for this query
+            currentXor ^= nums[nums.size() - 1 - i]; // Remove the last element
         }
         
         return answer;
