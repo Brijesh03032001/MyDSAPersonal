be1a04d084cee6a12b9eba0640c3ee00a16345a3
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-06-28 16:31:41 +0000

    Add LeetCode: 3174 - Minimum Number of Changes to Make Binary String Beautiful (cpp)

--- problems/Medium/3174-minimum-number-of-changes-to-make-binary-string-beautiful/solution_cpp.cpp
+++ problems/Medium/3174-minimum-number-of-changes-to-make-binary-string-beautiful/solution_cpp.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int te = 0;
-        for (int i = 0; i < s.size() - 1; i += 2) {
-            if ((s[i] == '0' && s[i + 1] == '1') || (s[i] == '1' && s[i + 1] == '0')) {
+        int te=0;
+        for(int i=0;i<s.size()-1;i+=2)
+        {
+            char c1= s[i];
+            char c2 = s[i+1];
+            string s1 = "";
+            s1+=c1;
+            s1+=c2;
+            if(s1 == "01" || s1=="10")
+            {
                 te++;
             }
         }
