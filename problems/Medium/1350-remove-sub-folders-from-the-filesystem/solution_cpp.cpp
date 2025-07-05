abdedaa533ae5d1c4383fadacecd09f2a9885cdd
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-07-05 09:45:49 +0000

    Add LeetCode: 1350 - Remove Sub-Folders from the Filesystem (cpp)

--- problems/Medium/1350-remove-sub-folders-from-the-filesystem/solution_cpp.cpp
+++ problems/Medium/1350-remove-sub-folders-from-the-filesystem/solution_cpp.cpp
@@ -1,26 +1,29 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
+        // Sort the folders lexicographically so parent folders come before their subfolders
         sort(folder.begin(), folder.end());
-        vector<string> res;
-        for (const auto& f : folder) {
-            if (res.empty()) {
-                res.push_back(f);
-            } else {
-                const string& prev = res.back();
-                if (f.find(prev) == 0 && f.size() > prev.size() && f[prev.size()] == '/') {
-                    continue;
-                } else {
-                    res.push_back(f);
-                }
+
+        // Initialize result vector with the first folder
+        vector<string> ans;
+        ans.push_back(folder[0]);
+
+        // Iterate through remaining folders starting from index 1
+        for(int i = 1; i < folder.size(); i++) {
+            // Get the last added folder path and add a trailing slash
+            // This helps in comparing if current folder is a subfolder
+            string lastFolder = ans.back();
+            lastFolder.push_back('/');
+
+            // Compare current folder with last added folder
+            // compare(0, lastFolder.size(), lastFolder) checks if folder[i] starts with lastFolder
+            // If it doesn't start with lastFolder (returns != 0), then it's not a subfolder
+            if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
+                // If not a subfolder, add to result
+                ans.push_back(folder[i]);
             }
         }
-        return res;
+
+        return ans;
     }
 };
