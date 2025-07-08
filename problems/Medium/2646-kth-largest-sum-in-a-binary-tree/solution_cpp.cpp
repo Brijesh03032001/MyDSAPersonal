7024ba667dd19d3b62300d5b2064998a9a0908fd
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-07-08 11:31:21 +0000

    Add LeetCode: 2646 - Kth Largest Sum in a Binary Tree (cpp)

--- problems/Medium/2646-kth-largest-sum-in-a-binary-tree/solution_cpp.cpp
+++ problems/Medium/2646-kth-largest-sum-in-a-binary-tree/solution_cpp.cpp
@@ -12,17 +12,13 @@
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-            //  vector<long long> vc;
-             priority_queue< long long , vector<long long> , greater<long long>> pq;
-
+             vector<long long> vc;
              if(root == NULL)
              {
                 return -1;
              }
              queue<TreeNode*> qu;
              qu.push(root);
-             int i=0;
-             int level = 0;
              while(!qu.empty())
              {
                 int n = qu.size();
@@ -41,25 +37,11 @@ public:
                         qu.push(q->right);
                      }
                 }
-                if(i<k)
-                {
-                    pq.push(temp);
-                    i++;
-                }
-                else
-                {
-                    if(pq.top()< temp)
-                    {
-                        pq.pop();
-                        pq.push(temp);
-                    }
-                }
-                //vc.push_back(temp);
-                level++;
+                vc.push_back(temp);
              }
 
-            //  sort(vc.rbegin(), vc.rend());
-             if(level<k) return -1;
-             return pq.top();
+             sort(vc.rbegin(), vc.rend());
+             if(vc.size()<k) return -1;
+             return vc[k-1];
     }
 };
