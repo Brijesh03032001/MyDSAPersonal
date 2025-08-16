31c8468295c62fe2ea03a8f454043e7c25205e12
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-08-16 13:14:47 +0000

    Add LeetCode: 238 - Product of Array Except Self (cpp)

--- problems/Medium/238-product-of-array-except-self/solution_cpp.cpp
+++ problems/Medium/238-product-of-array-except-self/solution_cpp.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
-        int n = nums.size();
-        vector<long long> lp(n+1, 1);
-        vector<long long> rp(n+1, 1);
-        long long lpro = 1;
-        long long rpro = 1;
-        for(int i=0;i<n; i++)
+        vector<int>pre;
+        vector<int>pos;
+        int pro=1;
+        for(int i=0;i<nums.size();i++)
         {
-            lpro *= nums[i];
-            lp[i+1] = lpro;
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            rpro *= nums[i];
-            rp[i] = rpro;
-        }
-        // for(int i =0 ;i < n+1 ; i++)
-        // {
-        //     cout << lp[i] << "**";
+            pre.push_back(pro);
+            pro= pro* nums[i];
 
-        // }
-        // cout << "\n";
-        // for(int i =0 ;i < n+1 ; i++)
-        // {
-        //     cout << rp[i] << "**";
-            
-        // }
-        vector<int>res(n);
-        for(int i=0;i<n;i++)
+        }
+        pro=1;
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            res[i] = lp[i] * rp[i+1];
+            pos.push_back(pro);
+            pro= pro* nums[i];
+            
+        }
+        reverse(pos.begin(), pos.end());
+        vector<int>res(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            res[i] = pre[i] * pos[i];
         }
         return res;
     }
