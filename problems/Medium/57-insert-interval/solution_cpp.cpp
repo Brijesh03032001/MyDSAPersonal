5baa83f2cf3fcbddc6b5f82d3ba1651a50ecef64
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-08-26 07:16:45 +0000

    Add LeetCode: 57 - Insert Interval (cpp)

--- problems/Medium/57-insert-interval/solution_cpp.cpp
+++ problems/Medium/57-insert-interval/solution_cpp.cpp
@@ -1,5 +1,26 @@
-// LeetCode submission — code could not be fetched automatically.
-// To fill this in, open the submission on LeetCode and paste your code here.
-// Submission key: insert-interval__1710705191
-// Language: cpp
-// Status: Accepted
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+        vector<int> pre = intervals[0];
+        vector<vector<int>> res;
+        for(int i=1;i<intervals.size();i++)
+        {
+            vector<int>curr = intervals[i];
+            if(pre[1] < curr[0])
+            {
+                res.push_back(pre);
+                pre= curr;
+            }
+            else
+            {
+                pre[1]= max(pre[1], curr[1]);
+            }
+        }
+        res.push_back(pre);
+        
+        return res;
+    }
+};
