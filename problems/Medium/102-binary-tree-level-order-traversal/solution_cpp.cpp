aff5cb68799b06dff712cf33dfc5941faf333b2c
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-08-30 14:23:12 +0000

    Add LeetCode: 102 - Binary Tree Level Order Traversal (cpp)

---
+++ problems/Medium/102-binary-tree-level-order-traversal/solution_cpp.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>res;
+        if(root == NULL) return res;
+        queue<TreeNode*>que;
+        que.push(root);
+        while(!que.empty())
+        {
+            vector<int>temp;
+            int n = que.size();
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* node = que.front();
+                que.pop();
+                if(node->left != NULL) que.push(node->left);
+                if(node->right != NULL) que.push(node->right);
+                temp.push_back(node->val);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
