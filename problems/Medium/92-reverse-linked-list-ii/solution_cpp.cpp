16199157f994fbdd7e1ab8bcb9152b70364fff69
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-10-01 11:27:00 +0000

    feat(92): Reverse Linked List II [Medium] (cpp)

---
+++ problems/Medium/92-reverse-linked-list-ii/solution_cpp.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode* dummy= new ListNode(0);
+        dummy->next= head;
+        ListNode* curr=head;
+        ListNode* preLeft= dummy;
+        for(int i=0;i<left-1;i++)
+        {
+            preLeft= curr;
+            curr= curr->next;
+        }
+        ListNode* pre= NULL;
+        
+        for(int i=0;i<right-left+1;i++)
+        {
+            ListNode* temp= curr->next;
+            curr->next= pre;
+            pre= curr;
+            curr= temp;
+        }
+        
+        preLeft->next->next=curr;
+        preLeft->next= pre;
+        return dummy->next;
+    }
+};
