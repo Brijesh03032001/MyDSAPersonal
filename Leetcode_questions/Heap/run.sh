2b22888cbb6552c25474c1e67276ba3f7974b64e
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-01-17 03:48:50 -0700

    feat: add heap compilation and execution script for easy testing

---
+++ Leetcode_questions/Heap/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the filename without extension
+filename=$(basename "$1" .cpp)
+
+# Compile and run with input/output redirection
+g++ -o "$filename" "$1" && ./"$filename" < ../../input.txt > ../../output.txt
+
+echo "Program executed successfully!"
+echo "Input from: ../../input.txt"
+echo "Output to: ../../output.txt"
