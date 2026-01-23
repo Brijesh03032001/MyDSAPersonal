82be2b05f98e32f2439966c89b5cff518298d43e
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-01-23 07:32:00 +0000

    feat(783): Search in a Binary Search Tree [Easy] (cpp)

---
+++ problems/Easy/783-search-in-a-binary-search-tree/solution_cpp.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL) 
+        {
+          return NULL;
+        }
+      
+      
+      TreeNode* ans= new TreeNode();   
+      if(root->val == val)
+      {
+        return root;
+      }
+      else if(root->val < val)
+      {
+        ans= searchBST(root->right,val);
+        if(ans!=NULL) return ans;
+      }
+      else 
+      {
+       ans= searchBST(root->left,val);
+        if(ans!=NULL) return ans;
+      }
+      return NULL;
+    }
+};
