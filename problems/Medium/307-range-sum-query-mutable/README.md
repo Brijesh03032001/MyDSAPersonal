96b47219e9e15255cf4c616e006e455d2c765d07
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-02-08 02:34:00 +0000

    feat(307): Range Sum Query - Mutable [Medium] (cpp)

---
+++ problems/Medium/307-range-sum-query-mutable/README.md
@@ -0,0 +1,41 @@
+# Range Sum Query - Mutable
+
+**Difficulty:** Medium  
+**URL:** [range-sum-query-mutable](https://leetcode.com/problems/range-sum-query-mutable/)  
+**Tags:** Array, Divide and Conquer, Design, Binary Indexed Tree, Segment Tree
+
+---
+
+## My Notes
+
+<!-- Add your personal notes here -->
+
+## Time Complexity
+
+<!-- e.g. O(n log n) -->
+
+## Space Complexity
+
+<!-- e.g. O(n) -->
+
+---
+
+## Problem Statement
+
+Given an integer array nums, handle multiple queries of the following types:
+
+
+	Update the value of an element in nums.
+	Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+
+
+Implement the NumArray class:
+
+
+	NumArray(int[] nums) Initializes the object with the integer array nums.
+	void update(int index, int val) Updates the value of nums[index] to be val.
+	int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+
+&nbsp;
+Example 1
---
+++ problems/Medium/307-range-sum-query-mutable/solution_cpp.cpp
@@ -0,0 +1,64 @@
+class NumArray {
+public:
+    vector<int> a;
+    vector<int> t;
+    int n;
+    
+    void init ( int id, int b, int e ) {
+        if ( b == e ) {
+            t[id] = a[b];
+            return;
+        }
+        
+        //int mid = ( b + e ) >> 1;
+        int mid = ( b + e ) /2;
+        init ( id*2+1, b, mid );
+        init ( id*2+2, mid+1, e );
+        
+        t[id] = t[id*2+1] + t[id*2+2];
+    } 
+    //update
+    void upd ( int id, int b, int e, int i, int val ) {
+        
+        if ( b == e && i == b ) {
+            t[id] = val;
+            a[b]=val;
+            return;
+        }
+        
+       // if ( i > e || i < b ) return;
+        
+        int mid = ( b + e ) /2;
+      if(i<=mid) upd ( id*2+1, b, mid, i, val );
+       else  upd ( id*2+2, mid+1, e, i, val );
+        
+        t[id] = t[id*2+1] + t[id*2+2];
+    }
+    //range helper
+    int ask ( int id, int b, int e, int i, int j ) {
+        if ( b >= i && e <= j ) return t[id];
+        if ( i > e || j < b ) return 0;
+        
+        int mid = ( b + e ) >> 1;
+        int ret1 = ask ( id*2+1, b, mid, i, j);
+        int ret2 = ask ( id*2+2, mid+1, e, i, j );
+        
+        return ret1+ret2;
+    }
+    
+    NumArray(vector<int>& nums) {
+        a = nums;
+        n = nums.size();
+        t.resize( n*4 +1);
+        
+        init ( 0, 0, n-1 );
+    }
+    
+    void update(int index, int val) {
+        upd ( 0, 0, n-1, index, val );
+    }
+    
+    int sumRange(int left, int right) {
+        return ask ( 0, 0, n-1, left, right );
+    }
+};
