ed441eb56b74129b3cc18be20906352df132461c
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-15 03:55:49 -0700

    Add LeetCode: 695 - Max Area of Island (cpp)

--- problems/Medium/695-max-area-of-island/solution_cpp.cpp
+++ problems/Medium/695-max-area-of-island/solution_cpp.cpp
@@ -1,49 +1,30 @@
 class Solution {
 public:
-
-    vector<pair<int,int>> pa { {0,1} , {0,-1} , {1,0} , {-1,0}};
-    int solve(int i, int j, int r, int c, vector<vector<int>>& grid , vector<vector<bool>>& vis)
-    {
-        if(i<0 || j<0 || i>=r || j>=c || grid[i][j] == 0 || vis[i][j] == true)
-        {
-            return 0;
-        }
-        int cu = grid[i][j];
-        vis[i][j] = true;
-        for(auto it : pa)
-        {
-            int f = it.first;
-            int l = it.second;
-            cu += solve(i+f, j+l , r, c , grid , vis);
-        }
-        return cu;
-    }
-  
-
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-          int r = grid.size();
-        int c = grid[0].size();
-           vector<vector<bool>> vis(r, vector<bool>(c, false));
-        
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                vis[i][j] = false;
-            }
-        }
-        int m = 0;
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                if(grid[i][j] != 0 && vis[i][j] == false)
-                {
-                       m = max(m, solve(i,j,r,c,grid,vis));
+        int rows = grid.size(), cols = grid[0].size();
+       
+        int maxIsland = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    maxIsland = max(maxIsland, dfs(grid, r, c));
                 }
             }
         }
-        return m;
 
+        return maxIsland;        
+    }
+
+private:
+    int dfs(vector<vector<int>>& grid, int r, int c) {
+        int rows = grid.size(), cols = grid[0].size();
+        if (r < 0 || r >= rows || c < 0 || c >= cols ||  grid[r][c] != 1) {
+            return 0;
+        }
+
+        grid[r][c] = 2;
+        return 1 + dfs(grid, r + 1, c) + dfs(grid, r - 1, c)
+                 + dfs(grid, r, c + 1) + dfs(grid, r, c - 1);
     }
 };
