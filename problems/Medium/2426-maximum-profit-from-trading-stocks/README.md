0b5a84afdddfb1722797007356e92d77b74fbed8
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-14 23:32:51 -0700

    Add LeetCode: 2426 - Maximum Profit From Trading Stocks (cpp)

---
+++ problems/Medium/2426-maximum-profit-from-trading-stocks/README.md
@@ -0,0 +1,25 @@
+# Maximum Profit From Trading Stocks
+
+**Difficulty:** Medium  
+**URL:** [https://leetcode.com/problems/maximum-profit-from-trading-stocks](https://leetcode.com/problems/maximum-profit-from-trading-stocks)  
+**Tags:** Array, Dynamic Programming
+
+---
+
+## My Notes
+
+<!-- Add your personal notes here -->
+
+## Time Complexity
+
+<!-- e.g. O(n log n) -->
+
+## Space Complexity
+
+<!-- e.g. O(n) -->
+
+---
+
+## Problem Statement
+
+
---
+++ problems/Medium/2426-maximum-profit-from-trading-stocks/solution_cpp.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int sol(vector<int>& arr, vector<int>& cost, int W, int i)
+    {
+        if(i == 0 ) return 0;
+        if(W < 0) return 0;
+        if(dp[i][W] != -1)
+        {
+            return dp[i][W];
+        }
+        if(arr[i-1] > W)
+        {
+             return dp[i][W] = sol(arr, cost, W, i-1);
+
+        }
+        else
+        {
+             int take = cost[i-1] + sol(arr, cost, W - arr[i-1] , i-1);
+             int not_take = sol(arr, cost, W, i-1);
+             int res = max(take, not_take);
+             return dp[i][W] = res;
+        }
+        return 0;
+    }
+    int maximumProfit(vector<int>& present, vector<int>& future, int budget) {
+
+        int n = present.size();
+        vector<int>cost(n, -1);
+        // if(budget == 0)
+        // {
+        //      int ref_su = 0;
+        //      for(int i=0;i<n;i++)
+        //      {
+        //         if( present[i] == 0)
+        //         {
+        //              ref_su += future[i];
+        //         }
+        //        return ref_su;
+
+        //      }
+        //  }
+        for(int i=0;i<n;i++)
+        {
+            cost[i] = future[i] - present[i];
+
+        }
+        memset(dp, -1, sizeof(dp));
+        return sol(present, cost, budget, n);
+    }
+};
