05d51c31f5e6c16dd3fb7a5cdc17bd503245cf9f
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-14 23:48:12 -0700

    Add LeetCode: 1544 - Count Good Nodes in Binary Tree (cpp)

---
+++ problems/Medium/1544-count-good-nodes-in-binary-tree/solution_cpp.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize("O3,unroll-loops,fast-math")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,abm")
+
+static const auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+/*
+
+Above lines of code enable maximum compiler optimizations (O3, loop 
+unrolling, fast-math) and CPU-specific instructions (AVX2, 
+BMI) to speed up execution, while the I/O tweaks 
+(sync_with_stdio(false)) reduce overhead for faster input/
+output operations in competitive programming. Together, 
+they minimize runtime by optimizing both computation and I/O.
+
+*/
+class Solution {
+public:
+ int goodNodes(TreeNode* root,int maxVal=INT_MIN) {
+        if(!root)return 0;
+        
+        // maxVal=max(maxVal,root->val);
+        int res = 0;
+        if(root->val >= maxVal) {
+            res = 1;
+            maxVal = root->val;
+        }
+       int l= goodNodes(root->left,maxVal);
+       int r=  goodNodes(root->right,maxVal);
+        res += l;
+        res += r;
+        return res;
+    }
+   
+};
