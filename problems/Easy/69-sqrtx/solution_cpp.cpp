3a673dd0c45a535ed619a5814ed5a8fd5df04855
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-15 03:55:54 -0700

    Add LeetCode: 69 - Sqrt(x) (cpp)

--- problems/Easy/69-sqrtx/solution_cpp.cpp
+++ problems/Easy/69-sqrtx/solution_cpp.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int lo=0;
-        int high = x;
+        if(x == 0) return 0;
+        int l=1;
+        int h = x;
         int ans = -1;
-        while(lo<=high)
+        while(l <= h)
         {
-            long long int mid = lo + (high-lo)/2;
-            if(mid*mid > x)
+            long long mid = l + (h-l)/2;
+            if( mid * mid <= x)
             {
-                high = mid-1;
+                   ans = mid;
+                   l = mid + 1;
             }
             else
             {
-                lo= mid +1 ;
-                ans = mid;
+                h = mid -1;
             }
         }
         return ans;
