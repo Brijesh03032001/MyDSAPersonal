cadadfb3f00cdad35f46577156ffa93edf7f6dca
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-14 23:33:02 -0700

    Add LeetCode: 907 - Koko Eating Bananas (cpp)

---
+++ problems/Medium/907-koko-eating-bananas/solution_cpp.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool ableToEat(int speed, vector<int>&piles, int h)
+    {
+        long long ref_hour = 0;
+        for(int i = 0; i < piles.size(); i++)
+        {
+            ref_hour += ceil((double)piles[i] / speed);
+        }
+        if(ref_hour <= h)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo = 1;
+        int hi = *max_element(piles.begin(), piles.end());
+        int ans = hi;
+
+        while(lo <= hi)
+        {
+            int mi = lo + (hi - lo)/2;
+
+            if(ableToEat(mi, piles, h))
+            {
+                ans = mi;
+                hi = mi - 1;
+            }
+            else
+            {
+                lo = mi + 1;
+            }
+        }
+        return ans;
+    }
+};
