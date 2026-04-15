283a6057b101c2ada519e2ed374043fe1d7488df
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-15 03:55:58 -0700

    Add LeetCode: 4141 - Count Elements With at Least K Greater Values (cpp)

--- problems/Medium/4141-count-elements-with-at-least-k-greater-values/solution_cpp.cpp
+++ problems/Medium/4141-count-elements-with-at-least-k-greater-values/solution_cpp.cpp
@@ -4,15 +4,15 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         
-        int count = 0;
+        int ans_count = 0;
         
         for (int i = 0; i < n; i++) {
-            // first element greater than nums[i]
+          
             int idx = upper_bound(nums.begin(), nums.end(), nums[i]) - nums.begin();
-            int greater = n - idx;
-            if (greater >= k) count++;
+            int an = n - idx;
+            if (an >= k) ans_count++;
         }
         
-        return count;
+        return ans_count;
     }
 };
