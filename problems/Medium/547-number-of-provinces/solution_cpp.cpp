23d11636c6a5169b390f18d9ed96e61f9e756037
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-15 03:56:17 -0700

    Add LeetCode: 547 - Number of Provinces (cpp)

--- problems/Medium/547-number-of-provinces/solution_cpp.cpp
+++ problems/Medium/547-number-of-provinces/solution_cpp.cpp
@@ -1,5 +1,23 @@
-// LeetCode submission — code could not be fetched automatically.
-// To fill this in, open the submission on LeetCode and paste your code here.
-// Submission key: number-of-provinces__1750789079
-// Language: cpp
-// Status: Accepted
+class Solution {
+public:
+    void dfs(vector<vector<int>>& isConnected, vector<int> &vis, int i, int n){
+        vis[i]=1;
+        for(int j=0; j<n; j++){
+            if(isConnected[i][j]==1 && vis[j]==0){
+                dfs(isConnected, vis, j, n);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<int> vis(n,0);
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                dfs(isConnected, vis, i, n);
+                cnt+=1;
+            }
+        }
+        return cnt;
+    }
+};
