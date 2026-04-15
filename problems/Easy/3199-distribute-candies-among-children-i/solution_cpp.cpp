8c71c3229bc85ddb4914c42f29b709c442d2dadc
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-14 23:49:06 -0700

    Add LeetCode: 3199 - Distribute Candies Among Children I (cpp)

---
+++ problems/Easy/3199-distribute-candies-among-children-i/solution_cpp.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int distributeCandies(int n, int limit) {
+        int co=0;
+        for(int i=0;i<=limit;i++)
+        {
+            for(int j=0;j<=limit;j++)
+            {
+                int k = n - i - j;
+                if(k>= 0 && k<=limit)
+                {
+                    co++;
+                }
+            }
+        }
+        return co;
+    }
+};
