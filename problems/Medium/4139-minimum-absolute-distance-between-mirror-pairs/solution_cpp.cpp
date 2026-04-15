592602f5a925323a95539a81395ffa6a13a04721
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-14 23:45:19 -0700

    Add LeetCode: 4139 - Minimum Absolute Distance Between Mirror Pairs (cpp)

--- problems/Medium/4139-minimum-absolute-distance-between-mirror-pairs/solution_cpp.cpp
+++ problems/Medium/4139-minimum-absolute-distance-between-mirror-pairs/solution_cpp.cpp
@@ -15,22 +15,22 @@ public:
         const int INF = 1e9;
         int ans = INF;
 
-       
+        // map from reversed value -> latest index i where reverse(nums[i]) == key
         unordered_map<long long, int> seenRevIndex;
         seenRevIndex.reserve(n * 2);
 
         for (int j = 0; j < n; ++j) {
-            
+            // check if there's any earlier i with reverse(nums[i]) == nums[j]
             auto it = seenRevIndex.find(nums[j]);
             if (it != seenRevIndex.end()) {
-                int i = it->second;      
+                int i = it->second;        // guaranteed i < j
                 ans = min(ans, j - i);
-                if (ans == 1) return 1;    
+                if (ans == 1) return 1;    // early exit, can't get smaller than 1
             }
 
-           
+            // add current index as candidate for future j's:
             long long rev_j = reverseInt(nums[j]);
-            
+            // store latest index for this reversed value (we want the closest i < future j)
             seenRevIndex[rev_j] = j;
         }
 
