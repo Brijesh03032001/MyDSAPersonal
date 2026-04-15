9fa494ef3323f662427130ee04aeb2d54f2c0243
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-14 23:47:18 -0700

    Add LeetCode: 1264 - Maximum Number of Words You Can Type (cpp)

---
+++ problems/Easy/1264-maximum-number-of-words-you-can-type/solution_cpp.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters){
+        unordered_set<char> st;
+        int cnt = 0;
+
+        for(auto& it : brokenLetters){
+            st.insert(it);
+        }
+
+        stringstream ss(text);
+
+        string word;
+        vector<string> words;
+
+        while(ss >> word){
+            words.push_back(word);
+        }
+
+        for(auto w: words)
+        {
+            for(char c : w)
+            {
+                if(st.find(c) != st.end())
+                {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        return words.size() - cnt;
+    }
+};
