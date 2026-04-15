6e37946578c3b6875dc65c302717bde300f4699d
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-14 23:48:47 -0700

    Add LeetCode: 11 - Container With Most Water (cpp)

--- problems/Medium/11-container-with-most-water/solution_cpp.cpp
+++ problems/Medium/11-container-with-most-water/solution_cpp.cpp
@@ -1,21 +1,5 @@
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int res = 0;
-        int p2= height.size()-1;
-        int p1=0;
-        while(p2>p1)
-        {
-            res = max(res, (p2-p1)*min(height[p2],height[p1]));
-            if(height[p1] > height[p2])
-            {
-                p2--;
-            }
-            else
-            {
-                p1++;
-            }
-        }
-        return res;
-    }
-};
+// LeetCode submission — code could not be fetched automatically.
+// To fill this in, open the submission on LeetCode and paste your code here.
+// Submission key: container-with-most-water__1749854973
+// Language: cpp
+// Status: Accepted
