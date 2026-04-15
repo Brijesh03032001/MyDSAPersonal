e234115e2d35524ca881a9025bf14dbe7a8128e0
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-15 03:55:44 -0700

    Add LeetCode: 435 - Non-overlapping Intervals (cpp)

--- problems/Medium/435-non-overlapping-intervals/solution_cpp.cpp
+++ problems/Medium/435-non-overlapping-intervals/solution_cpp.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    static bool com(vector<int>& a, vector<int>& b) {
-        return a[1] < b[1];  // Sort by end time in ascending order (Greedy approach)
-    }
-
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return 0;  // Handle empty case
-
-        sort(intervals.begin(), intervals.end(), com);  // Sorting using the static comparator
-        
-        int end = intervals[0][1];
-        int count = 0;
-
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] < end) {  
-                count++;  // Overlapping interval found, remove it
-            } else {
-                end = intervals[i][1];  // Update end time for the next comparison
+        sort(intervals.begin(), intervals.end());
+        int L=0;
+        int i=1;
+        int n = intervals.size();
+        if(n == 1) return 0;
+        int c = 0; // count of overlapping intervals
+        while(i<n)
+        {
+            // safe condition
+            if(intervals[L][1] <= intervals[i][0])
+            {
+                L=i;
+                i++;
+            }
+            else
+            {
+                if(intervals[L][1] <= intervals[i][1])
+                {
+                    
+                    i++;
+                }
+                else
+                {
+                   
+                    L = i;
+                    i++;
+                }
+                c++;
             }
         }
-        return count;
+        return c;
     }
 };
