00dc114ac3c50324d03e8670dc9240b0323655fe
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-04-15 03:56:14 -0700

    Add LeetCode: 1544 - Count Good Nodes in Binary Tree (cpp)

--- problems/Medium/1544-count-good-nodes-in-binary-tree/solution_cpp.cpp
+++ problems/Medium/1544-count-good-nodes-in-binary-tree/solution_cpp.cpp
@@ -1,26 +1,18 @@
-#pragma GCC optimize("O3,unroll-loops,fast-math")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,abm")
-
-static const auto _ = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
-/*
-
-Above lines of code enable maximum compiler optimizations (O3, loop 
-unrolling, fast-math) and CPU-specific instructions (AVX2, 
-BMI) to speed up execution, while the I/O tweaks 
-(sync_with_stdio(false)) reduce overhead for faster input/
-output operations in competitive programming. Together, 
-they minimize runtime by optimizing both computation and I/O.
-
-*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
- int goodNodes(TreeNode* root,int maxVal=INT_MIN) {
-        if(!root)return 0;
+    int goodNodes(TreeNode* root, int maxVal = INT_MIN) {
+         if(!root)return 0;
         
         maxVal=max(maxVal,root->val);
        int l= goodNodes(root->left,maxVal);
@@ -28,5 +20,4 @@ public:
         if(root->val>=maxVal) return l+r+1;
         return l+r;
     }
-   
 };
