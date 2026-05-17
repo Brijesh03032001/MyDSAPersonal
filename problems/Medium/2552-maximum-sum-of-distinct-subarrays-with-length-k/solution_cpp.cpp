9a2106e5c225bb854006366557a397a3fefbfd95
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-05-16 21:05:55 -0700

    feat(2552): Maximum Sum of Distinct Subarrays With Length K [Medium] (cpp)

--- problems/Medium/2552-maximum-sum-of-distinct-subarrays-with-length-k/solution_cpp.cpp
+++ problems/Medium/2552-maximum-sum-of-distinct-subarrays-with-length-k/solution_cpp.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long res = 0;
-        unordered_map<int, int> count;
-        long long curSum = 0;
+        int p1= 0;
+        int p2= 0;
+        long long su = 0;
+        unordered_map<int,int> mp;
+        long long temp_su = 0;
+        int n = nums.size();
         
-        int l = 0;
-        for (int r = 0; r < nums.size(); r++) {
-            curSum += nums[r];
-            count[nums[r]]++;
-            
-            if (r - l + 1 > k) {
-                count[nums[l]]--;
-                if (count[nums[l]] == 0) {
-                    count.erase(nums[l]);
-                }
-                curSum -= nums[l];
-                l++;
+        while(p2 < n && p2 - p1 + 1 <= k)
+        {
+
+            if(mp.find(nums[p2]) == mp.end())
+            {
+                 mp[nums[p2]] = p2;
             }
-            
-            if (count.size() == k && (r - l + 1) == k) {
-                res = max(res, curSum);
+            else
+            {
+                while(mp.find(nums[p2]) != mp.end())
+                {
+                    mp.erase(nums[p1]);
+                    temp_su -= nums[p1];
+                    p1++;
+                }
+                mp[nums[p2]] = p2;
+            }
+            temp_su += nums[p2];
+            if(p2-p1+1 == k)
+            {
+                su = max(su,temp_su); 
+            }
+
+            p2++;
+
+            if(p2 - p1 + 1 > k)
+            {
+               temp_su -= nums[p1];
+               if(mp.find(nums[p1]) != mp.end())
+               {
+                 mp.erase(nums[p1]);
+               }
+               p1++;
+               
             }
         }
-        
-        return res;
+
+
+        return su;
     }
 };
