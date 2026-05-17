845f6f807ff3f05ba1fc3f6474af3fd182c25a4d
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-05-16 21:05:53 -0700

    feat(3980): Best Time to Buy and Sell Stock using Strategy [Medium] (cpp)

--- problems/Medium/3980-best-time-to-buy-and-sell-stock-using-strategy/solution_cpp.cpp
+++ problems/Medium/3980-best-time-to-buy-and-sell-stock-using-strategy/solution_cpp.cpp
@@ -1,48 +1,50 @@
 class Solution {
 public:
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
-        long long right_part = 0;
-        int n = prices.size();
-
-        // base baad mein
-        long long left_part = 0;
-        long long mid_part = 0;
-        int div = k/2;
-        for(int i=k;i<n;i++)
-            {
-                right_part += (prices[i]*strategy[i]);
-            }
-        for(int i=div;i<k;i++)
-            {
-                mid_part += prices[i];
-            }
-        long long ref = 0;
-        ref+= mid_part;
-        ref+= right_part;
-        ref+= left_part;
         
-        for(int i=k;i<n;i++)
-        {
-              right_part -= (prices[i]*strategy[i]);
-              mid_part += prices[i];
-            mid_part -= prices[i-div];
-            left_part = left_part + (prices[i-k]*strategy[i-k]);
-            long long temp = right_part + mid_part +left_part ;
-            if(temp> ref)
-            {
-                ref = temp;
-            }
-        }
-        long long base_res =0;
+        long long ref_su = 0;
+        long long temp_su = 0;
+        long long original_su =0;
+        int n= prices.size();
         for(int i=0;i<n;i++)
-            {
-                base_res += (prices[i]*strategy[i]);
-            }
-        if(base_res > ref)
         {
-            ref = base_res;
+            if(i>=k)
+            {
+                ref_su += (prices[i]*strategy[i]);
+            }
+
+            original_su += (prices[i]*strategy[i]);
         }
-        return ref;
         
+        for(int i=k/2;i<k;i++)
+        {
+            temp_su += prices[i];
+        }
+
+        int te = 0 + (k/2) - 1;
+       
+        long long su_till_now =0;
+        su_till_now += ref_su;
+        su_till_now += temp_su;
+        original_su = max(original_su, su_till_now );
+        // cout << original_su <<" ***";
+        // if(n<=2) return original_su;
+         for(int i=1;i<(n - (k) + 1);i++)
+        {
+            te = i + (k/2) - 1;
+            su_till_now += prices[i+ k - 1];
+            su_till_now -= ((long long)prices[i + k - 1] *
+                (long long)strategy[i + k - 1]);
+            su_till_now += ((long long)prices[i - 1] *
+                (long long)strategy[i  - 1]);
+            
+            su_till_now -= prices[te];
+            // cout << su_till_now << "***";
+            original_su = max(original_su, su_till_now );
+        }
+
+        return original_su;
+        
+
     }
 };
