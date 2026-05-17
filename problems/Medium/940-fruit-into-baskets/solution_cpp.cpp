c7ef7e0fadcd2ad5b88957814fbc9a5c604ea222
Brijesh03032001 <sainibrijesh01@gmail.com>
2026-05-16 21:05:52 -0700

    feat(940): Fruit Into Baskets [Medium] (cpp)

--- problems/Medium/940-fruit-into-baskets/solution_cpp.cpp
+++ problems/Medium/940-fruit-into-baskets/solution_cpp.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> mpp;
-
-        int i = 0, j = 0, res = 0;
-
-        
-
-        while(j < fruits.size())
-
+        unordered_set<int>st;
+        int n = fruits.size();
+        int p1 = 0, p2 = 0;
+        int le = 0;
+        priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>> > pq;
+        while(p2< n)
         {
-
-            mpp[fruits[j]]++;
-
-            
-
-            if(mpp.size() <= 2) res = max(res, j-i+1);
-
-            
-
-            else
-
+            if(st.find(fruits[p2]) == st.end())
             {
+                // unique mila
+                if(pq.size() == 2)
+                {
+                    pair<int,int> top_ele = pq.top();
+                    pq.pop();
+                    p1 = top_ele.first + 1;
+                    st.erase(top_ele.second);
+                    // pq.push({p2, fruits[i]});
+                }
+                pq.push({p2, fruits[p2]});
+                st.insert( fruits[p2]);
+            }
+            else
+            {
+                vector<pair<int,int>> temp;
+                while(!pq.empty() && pq.top().second != fruits[p2])
+                {
+                    pair<int,int> top_ele = pq.top();
+                    temp.push_back(top_ele);
+                    pq.pop();
+                }
+                pq.pop();
+                pq.push({p2, fruits[p2]});
+                if(temp.size() != 0) pq.push(temp[0]);
 
-                mpp[fruits[i]]--;
-
-                if(mpp[fruits[i]] == 0) mpp.erase(fruits[i]);
-
-                i++;
-
+                  
+                   
+                
+               
+                
             }
 
-            j++;
-
+            le = max(le, p2-p1+1);
+            p2++;
         }
-
-        return res; 
+        return le;
     }
 };
